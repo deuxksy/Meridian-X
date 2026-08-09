bb56b9ceb0dfdf805ad086fe1356671c56259d13
Crong <deuxksy@gmail.com>
2026-08-09 12:39:13 +0900

    fix: filter out static.mediafire.com ad links and non-HTTP protocol URLs

--- src/url_resolver/extractors/misskon.py
+++ src/url_resolver/extractors/misskon.py
@@ -1,12 +1,19 @@
 from bs4 import BeautifulSoup
 from url_resolver.extractors.base import BaseExtractor
 
+TARGET_DOMAINS = ["ouo.io", "ouo.press", "mediafire.com", "mega.nz", "gofile.io"]
+
+
 class MisskonParser(BaseExtractor):
     def extract_download_links(self, html_content: str) -> list[str]:
         soup = BeautifulSoup(html_content, "html.parser")
         links = []
         for a in soup.find_all("a", href=True):
-            href = a["href"]
-            if any(domain in href for domain in ["ouo.io", "ouo.press", "mediafire.com", "mega.nz"]):
+            href = a["href"].strip()
+            if any(domain in href for domain in TARGET_DOMAINS):
+                if "static.mediafire.com" in href or "af_link.php" in href:
+                    continue
+                if not href.startswith("http://") and not href.startswith("https://"):
+                    continue
                 links.append(href)
         return list(dict.fromkeys(links))
